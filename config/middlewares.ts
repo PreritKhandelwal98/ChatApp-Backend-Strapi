89b232a5879ca1eeef144204334190e164e33cb8
Prerit Khandelwal <prerit.web@gmail.com>
2025-02-14 10:49:25 +0530

    back to standard one for cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,15 +2,7 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ["*"], // For debugging, allow all origins (change later to specific domains)
-      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-      headers: ["*"], // Allow all headers (change later)
-      keepHeadersOnError: true,
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
